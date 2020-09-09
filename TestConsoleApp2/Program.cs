f8e95338299a41d7d56783a0830d3835011476a7
Edo90 <edward.colon26@gmail.com>
2020-09-09 11:47:58 -0400

    Add project files.

---
+++ TestConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this code could change
+            int code = 13;
+
+            Console.WriteLine("favor ingrese la palabra a codificar");
+            string word = Console.ReadLine();
+            char[] letters = word.ToCharArray();
+
+            string result = "";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (letters[i] + code <= 'z')
+                    result += (char)(letters[i] + code);
+                else
+                {
+                    int position = (int)(letters[i] - code);
+                    result += (char)(position);
+                }
+            }
+
+            Console.WriteLine($"Su palabra codificada es {result}");
+            Console.Read();
+        }
+    }
+}
